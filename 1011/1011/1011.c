5b1613215bcb4d6aadba8e04aae7aba1defb5b53
Na-hyunwoo <dusdjeks@naver.com>
2021-08-28 13:07:47 +0900

    1978 done

--- 1011/1011/1011.c
+++ 1011/1011/1011.c
@@ -2,12 +2,29 @@
 
 int main(){
 	int count;
-	int a, b;
+	int x, y;
 
 	scanf("%d", &count);
 
 	for (int i = 0; i < count; i++) {
-		scanf("%d %d", &a, &b);
+		scanf("%d %d", &x, &y);
+		if (y - x == 1) {
+			printf("1");
+			break;
+		}
+		if (y - x == 2) {
+			printf("2");
+			break;
+		}
+		if (y - x == 3 || y-x==4) {
+			printf("3");
+			break;
+		}
+		if (y - x == 5 ||y-x==6) {
+			printf("4");
+			break;
+		}
+
 
 	}
 
---
+++ 1978/1978/1978.c
@@ -0,0 +1,30 @@
+﻿#include <stdio.h>
+
+int main() {
+	int a,n;
+	int count ;
+
+	scanf("%d", &n);
+	count = n;
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &a);
+		if (a == 1) count--;
+		for (int j = 2; j < a; j++) {
+			if (a % j == 0) {
+				count--;
+				
+				break;
+			}
+			else {
+				continue;
+			}
+		}
+		
+	}
+
+
+	printf("%d", count);
+
+
+	return 0;
+}
