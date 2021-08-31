0312d120a1318e5dfa634ee70494ff8897af42bf
Na-hyunwoo <dusdjeks@naver.com>
2021-08-31 10:51:06 +0900

    4948 done

--- 4948/4948/4948.c
+++ 4948/4948/4948.c
@@ -1,29 +1,27 @@
 #include <stdio.h>
 
-/*
+
 int main() {
 	int n;
 	int sum;
-	int temp;
+
 
 	while (1) {
 		scanf("%d", &n);
+		if (n == 0) {
+			break;
+		}
 		if (n == 1) {
 			printf("1\n");
 		}
 		else {
+			sum = n - 1;
 			for (int i = n + 1; i < 2 * n; i++) {
-				temp = 0;
-				sum = 0;
 				for (int j = 2; j * j <= i; j++) {
 					if (i % j == 0) {
-						temp = 1;
+						sum--;
 						break;
 					}
-				}
-				if (temp == 0) {
-					printf("i:%d\n", i);
-					sum++;
 				}
 			}
 			printf("%d\n", sum);
@@ -38,27 +36,5 @@ int main() {
 
 	return 0;
 }
-*/
 
-int main() {
-	int n;
-	int temp;
-	int sum;
 
-	scanf("%d", &n);
-
-	for (int i = n + 1; i < 2*n; i++) {
-		sum = n-1;
-		temp = 0;
-		for (int j = 2; j  <= i; j++) {
-			if (i % j == 0) {
-				sum--;
-				break;
-			}
-		}
-
-	}
-
-	printf("%d\n", sum);
-
-}
