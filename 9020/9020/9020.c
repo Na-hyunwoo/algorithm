6551ad9c4afa988b5b55e4c26b0ee91b5d0b2d79
Na hyunwoo <dusdjeks@naver.com>
2021-09-02 11:01:27 +0900

    9020 try2

--- 9020/9020/9020.c
+++ 9020/9020/9020.c
@@ -1,65 +1,63 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int* list(int a) {
-	int *arr=(int *)malloc(sizeof(int)*a);
-	int len;
-	for (int i = 2; i < a; i++) {
-		arr[i-2] = i;
-	}
-
-	len = sizeof(arr);
-
-	for (int i = 0; i < len; i++) {
-		printf("arr[%d]:%d\n", i, arr[i]);
-	}
-
-	for (int i = 0; i < len; i++) {
-		for (int j = 2; j < arr[i]; j++) {
-			if (arr[i] % j == 0) {
-				for (int k = j; k < len-1; k++) {
-					arr[k] = arr[k + 1];
-				}
-				len--;
-			}
-		}
-	}
-
-	for (int i = 0; i < len; i++) {
-		printf("arr[%d]:%d\n", i, arr[i]);
-	}
-
-	return arr;
-}
-
-int main() {
-	int *arr;
-	arr = list(6);
-	for (int i = 0; i < sizeof(arr); i++) {
-		printf("%d\n", arr[i]);
-	}
-}
-
-/*
 int main() {
 	int n;
 	int a;
-	int arr[10000] = { 0, };
+	int *arr;
+	int x=0, y=0;
 
 	scanf("%d", &n);
 
 	for (int i = 0; i < n; i++) {
 		scanf("%d", &a);
-		for (int j = 2; j < a; j++) {
-			for (int k = 2; k < j; k++) {
-				if (j % k == 0) {
-					break;
+		int* arr = (int*)malloc(sizeof(int) * a);
+		int len;
+		for (int i = 2; i < a; i++) {
+			arr[i - 2] = i;
+		}
+
+		len = sizeof(arr);
+
+		for (int i = 0; i < len-1; i++) {
+			printf("arr[%d]:%d\n", i, arr[i]);
+		}
+
+		for (int i = 0; i < len; i++) {
+			for (int j = 2; j < arr[i]; j++) {
+				if (arr[i] % j == 0) {
+					for (int k = j; k < len - 1; k++) {
+						arr[k] = arr[k + 1];
+					}
+					len--;
 				}
 			}
 		}
+
+		for (int i = 0; i < len-1; i++) {
+			printf("arr[%d]:%d\n", i, arr[i]);
+		}
+
+		for (int i = 0; i < len - 1; i++) {
+			for (int j = i; j < len - 1; j++) {
+				if (arr[i] + arr[j] == a) {
+					if (arr[i] == arr[j]) {
+						printf("%d %d", arr[i],arr[j]);
+						break;
+					}
+					else {
+						x = arr[i];
+						y = arr[j];
+					}
+				}
+			}
+		}
+
+		printf("%d %d", x, y);
+		free(arr);
 	}
 
 	return 0;
 
 
 }
-*/
