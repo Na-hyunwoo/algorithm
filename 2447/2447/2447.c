2959bfaf492fd57a0534da7edf2c6bb3fede871a
Na hyunwoo <dusdjeks@naver.com>
2021-09-13 10:51:28 +0900

    2447 try

---
+++ 2447/2447/2447.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+int print(int a) {}
+
+
+int main() {
+	int n;
+
+	scanf("%d", &n);
+
+	if (n == 3) {
+		for (int i = 0; i < 1; i++) {
+			printf("***\n");
+			printf("* *\n");
+			printf("***");
+		}
+	}
+	else {
+
+	}
+	
+
+	return 0;
+}
+
+int printf(int a) {
+	int n;
+	for (int i = 0; i < n - 1; i++) {
+
+	}
+
+}
