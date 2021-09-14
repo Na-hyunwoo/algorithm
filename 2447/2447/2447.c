86b20bff63f20ac36302410649084941db04612b
Na hyunwoo <dusdjeks@naver.com>
2021-09-14 10:31:53 +0900

    2447 try2

--- 2447/2447/2447.c
+++ 2447/2447/2447.c
@@ -2,30 +2,21 @@
 int print(int a) {}
 
 
+
+//배열과 재귀함수를 사용해보자. 
 int main() {
 	int n;
 
 	scanf("%d", &n);
 
-	if (n == 3) {
-		for (int i = 0; i < 1; i++) {
-			printf("***\n");
-			printf("* *\n");
-			printf("***");
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			printf("*");
 		}
-	}
-	else {
-
+		printf("\n");
 	}
 	
 
 	return 0;
 }
 
-int printf(int a) {
-	int n;
-	for (int i = 0; i < n - 1; i++) {
-
-	}
-
-}
