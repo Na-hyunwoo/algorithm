6f2da2cc1f83ce81495bca7a15ff2c40a10a1cba
Na hyunwoo <dusdjeks@naver.com>
2021-09-15 11:00:16 +0900

    2447 try again

--- 2447/2447/2447.c
+++ 2447/2447/2447.c
@@ -1,22 +1,39 @@
 #include <stdio.h>
-int print(int a) {}
 
+char star[2200][2200];
 
+void print(int a);
 
-//배열과 재귀함수를 사용해보자. 
+//배열과 재귀함수를 사용해보자. 그리는 느낌으로 가자. 
+//아니야 결국에는 배열에 다 넣어야지 그림으로 표현될 수 있어. 
 int main() {
 	int n;
 
 	scanf("%d", &n);
 
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			printf("*");
+	if(n==3) paint(3);
+	else {
+		for (int i = 0; i < n/3; i++) {
+			for (int j = 0; j < n/3; j++) {
+				print();
+			}
 		}
-		printf("\n");
 	}
-	
+
 
 	return 0;
 }
 
+void paint(int a) {
+	int n = a;
+
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 3; j++) {
+			star[i][j] = '*';
+		}
+	}
+
+	star[1][1] = ' ';
+
+	
+}
