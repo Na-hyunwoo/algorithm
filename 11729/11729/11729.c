424b1329403953bf4c185994f72c4fec91514846
Na hyunwoo <dusdjeks@naver.com>
2021-09-17 11:37:14 +0900

    11729 try

--- 11729/11729/11729.c
+++ 11729/11729/11729.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
+#include <math.h>
 
 
 //이거는 뭐 어떻게 접근을 해야할지 감조차 잡히지가 않는데. 
 int main() {
 	int n;
+	int cnt;
 
 	scanf("%d", &n);
+	
+	cnt = pow(2, n) - 1;
 
+	printf("%d\n", cnt);
 
 	return 0;
 }
