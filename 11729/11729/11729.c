0d7a75858afacc2e04f711345d091b459f374a0e
Na hyunwoo <dusdjeks@naver.com>
2021-09-18 10:30:22 +0900

    11729 done

--- 11729/11729/11729.c
+++ 11729/11729/11729.c
@@ -3,6 +3,20 @@
 
 
 //이거는 뭐 어떻게 접근을 해야할지 감조차 잡히지가 않는데. 
+//재귀함수가 확실히 진짜 대박이긴 하네. 이게 아는거랑 모르는거랑 차이가 크긴하네. 
+
+void move(int n, int from, int to, int by) {
+	if (n == 1) {
+		printf("%d %d\n", from, to);
+		return ;
+	}
+
+	move(n - 1, from, by, to);
+	printf("%d %d\n", from, to);
+	move(n - 1, by, to, from);
+
+}
+
 int main() {
 	int n;
 	int cnt;
@@ -12,6 +26,8 @@ int main() {
 	cnt = pow(2, n) - 1;
 
 	printf("%d\n", cnt);
+
+	move(n, 1, 3, 2);
 
 	return 0;
 }
