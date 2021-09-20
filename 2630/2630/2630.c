0b5be7d1aa45b3bcfb3a2c867e30c1df597e0043
Na hyunwoo <dusdjeks@naver.com>
2021-09-20 10:48:35 +0900

    2630 try

--- 2630/2630/2630.c
+++ 2630/2630/2630.c
@@ -1,10 +1,28 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 int array[128][128];
+int blue = 0, white = 0;
 
 void check(int n,int x_s,int y_s) {
 	
+	bool zero, one;
+	zero = one = true;
+	for (int i = x_s; i < x_s + n; i++) {
+		for (int j = y_s; j < y_s + n; j++) {
+			if (array[i][j] == 1) zero = false;
+			if (array[i][j] == 0) one = false;
+		}
+	}
 
+	if (one == true) {
+		blue++;
+		return;
+	}
+	if (zero = true) {
+		white++;
+		return;
+	}
 
 	check(n / 2, x_s, y_s);
 	check(n / 2, x_s + n / 2, y_s);
@@ -23,6 +41,14 @@ int main() {
 		}
 	}
 
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			printf("%d ", array[i][j]);
+		}
+		printf("\n");
+	}
+
 	check(n,0,0);
 
+	printf("%d\n%d", white, blue);
 }
