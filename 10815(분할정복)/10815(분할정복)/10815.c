6f7d7dab40a3c76c7a883a02de6f0914f663a259
Na hyunwoo <dusdjeks@naver.com>
2021-09-23 10:56:26 +0900

    10815 try2

--- 10815(분할정복)/10815(분할정복)/10815.c
+++ 10815(분할정복)/10815(분할정복)/10815.c
@@ -3,13 +3,23 @@
 
 int* arr1, * arr2;
 
-void delete() {
+void findNumber(int i,int j, int n) {
+	int mid = i + j / 2;
+	if (i == j) {
+		if (n == arr1[i]) printf("1");
+		else { printf("0"); }
+	}
+	else {
+		findNumber(0, mid, n);
+		findNumber(mid + 1, j, n);
+	}
 
+	
 }
 
 int main() {
 	int n1, n2, j;
-	
+	int cnt;
 
 	scanf("%d", &n1);
 	arr1 = (int*)malloc(sizeof(int) * n1);
@@ -21,17 +31,10 @@ int main() {
 	arr2 = (int*)malloc(sizeof(int) * n2);
 	for (int i = 0; i < n2; i++) {
 		scanf("%d", &arr2[i]);
+		findNumber(0,n1-1,arr2[i]);
 	}
 
-	for (int i = 0; i < n2; i++) {
-		for (j = 0; j < n1; j++) {
-			if (arr2[i] == arr1[j]) {
-				printf("1 ");
-				break;
-			}
-		}
-		if(j==n1) printf("0 ");
-	}
+
 
 	return 0;
 }
