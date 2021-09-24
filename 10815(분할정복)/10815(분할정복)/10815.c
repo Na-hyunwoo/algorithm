0aed3c5b2b25df6e21138751c3df6d8dba28e3b5
Na hyunwoo <dusdjeks@naver.com>
2021-09-24 11:09:36 +0900

    10815 recursive method try3

--- 10815(분할정복)/10815(분할정복)/10815.c
+++ 10815(분할정복)/10815(분할정복)/10815.c
@@ -3,15 +3,22 @@
 
 int* arr1, * arr2;
 
+//하 뭐가 이렇게 잘 안되냐... 이게 재귀는 진짜 뜻대로 안돼. 재귀 어떻게 하는거냐고오오
 void findNumber(int i,int j, int n) {
+	int number=0;
 	int mid = i + j / 2;
 	if (i == j) {
-		if (n == arr1[i]) printf("1");
-		else { printf("0"); }
+		if (n == arr1[i]) number=1;
+	}
+	else if (i == j - 1) {
+		if (arr1[i] == n) number=1;
+		else if (arr1[j] == n) number=1;
 	}
 	else {
 		findNumber(0, mid, n);
 		findNumber(mid + 1, j, n);
+		if (number == 1) printf("1 ");
+		else printf("0 ");
 	}
 
 	
@@ -31,7 +38,7 @@ int main() {
 	arr2 = (int*)malloc(sizeof(int) * n2);
 	for (int i = 0; i < n2; i++) {
 		scanf("%d", &arr2[i]);
-		findNumber(0,n1-1,arr2[i]);
+		findNumber(0, n1 - 1, arr2[i]);
 	}
 
 
