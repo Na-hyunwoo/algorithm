c096210afed0f21cea0a67e1226d8ba6ff246905
Na hyunwoo <dusdjeks@naver.com>
2021-09-27 10:32:46 +0900

    10816 alomost done

--- 10816/10816/10816.c
+++ 10816/10816/10816.c
@@ -1,6 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int lower(int* arr,int size, int num) {
+	int start = 0;
+	int end = size - 1;
+	int mid;
+	while (start < end) {
+		mid = (start + end) / 2;
+		if (arr[mid] >= num) {
+			end = mid;
+		}
+		else {
+			start = mid + 1;
+		}
+	}
+
+	return end;
+}
+
+int upper(int *arr,int size, int num) {
+	int start = 0;
+	int end = size - 1;
+	int mid;
+	while (start < end) {
+		mid = (start + end) / 2;
+		if (arr[mid] > num) {
+			end = mid;
+		}
+		else {
+			start = mid + 1;
+		}
+	}
+
+	return end;
+}
+
+int b_search(int* arr, int size, int num) {
+	int a, b;
+	a = upper(arr, size, num);
+	b = lower(arr, size, num);
+	
+	printf("upper:%d \n", a);
+	printf("lower:%d \n", b);
+
+	return a - b;
+}
+
 int compare(const void* first, const void* second) {
 	if (*(int*)first > *(int*)second)
 		return 1;
@@ -21,14 +66,13 @@ int main() {
 	}
 	
 	qsort(arr1, n1, sizeof(int), compare);
-	for (int i = 0; i < n1; i++) {
-		printf("%d ", arr1[i]);
-	}
 
 	scanf("%d", &n2);
 	arr2 = (int*)malloc(sizeof(int) * n2);
 	for (int i = 0; i < n2; i++) {
 		scanf("%d", &arr2[i]);
+		printf("%d \n",b_search(arr1, n1, arr2[i]));
+
 	}
 
 
