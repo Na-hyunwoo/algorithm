b6108562ad8f892681a2fedf37f54143f67b3387
Na hyunwoo <dusdjeks@naver.com>
2021-10-09 10:56:10 +0900

    9184 done

--- 9184/9184/9184.c
+++ 9184/9184/9184.c
@@ -3,15 +3,15 @@
 int arr[21][21][21] = { 0, };
 
 int w(int a, int b, int c) {
-	
-	if (arr[a][b][c]!=0) {
-		return arr[a][b][c];
-	}
-	else if (a <= 0 || b <= 0 || c <= 0){
+
+	if (a <= 0 || b <= 0 || c <= 0){
 		return 1;
 	}
 	else if (a > 20 || b > 20 || c > 20){
 		return w(20, 20, 20);
+	}
+	else if (arr[a][b][c] != 0) {
+		return arr[a][b][c];
 	}
 	else if (a < b && b < c){
 		arr[a][b][c] = w(a, b, c - 1) + w(a, b - 1, c - 1) - w(a, b - 1, c);
