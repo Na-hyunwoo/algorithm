eab24136e390cfa113f4ed83f6a87420a133e5dc
unknown <dusdjeks@naver.com>
2021-10-30 10:38:22 +0900

    1931 try

--- 1931/1931/1931.c
+++ 1931/1931/1931.c
@@ -1,42 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
 int main() {
 
 	int n;
 	int count = 0;
-	unsigned int temp = 0;
 	unsigned int swap;
-	int* arr;
+	unsigned int temp;
+	int** arr;
 
 	scanf("%d", &n);
 
-	arr = (int*)malloc(sizeof(int) * 2 * n);
-
-	for (int i = 0; i < n*2;i++ ) {
-		scanf("%d", &arr[i]);
+	arr = (int**)malloc(sizeof(int) * n);
+	for (int i = 0; i < n; i++) {
+		arr[i] = (int*)malloc(sizeof(int) * 2);
 	}
 
-	//끝나는 시간이 빠른 기준으로 정렬하기
-	//n제곱으로 하면 시간초과 난다. 
-	for (int i = 1; i < n * 2; i+=2) {
-		for (int j = 1; j < n * 2-2; j += 2) {
-			if (arr[j] > arr[j+2]) {
-				swap = arr[j+2];
-				arr[j+2] = arr[j];
-				arr[j] = swap;
-				swap = arr[j +1];
-				arr[j + 1] = arr[j - 1];
-				arr[j - 1] = swap;
+	for (int i = 0; i < n;i++ ) {
+		scanf("%d %d", &arr[i][0],&arr[i][1]);
+	}
+
+
+	
+	//이중 배열을 시간 복잡도가 크지 않게 정렬을 해야 한다. 
+	for (int i = 0; i < n-1; i++) { //n-1번 반복. 
+		for (int j = 0; j < n - i-1; i++) {
+			if (arr[j][1] > arr[j + 1][1]) {
+				swap = arr[j][1];
+				arr[j][1] = arr[j + 1][1];
+				arr[j + 1][1] = swap;
+				swap = arr[j][0];
+				arr[j][0] = arr[j + 1][0];
+				arr[j + 1][0] = swap;
 			}
 		}
 	}
 
-	/*for (int i = 0; i < n * 2; i++) {
-		printf("%d ", arr[i]);
-	}*/
-
-	printf("\n");
+	//정렬이 잘 되었는지 확인 !
+	for (int i = 0; i < n; i++) {
+		printf("%d %d\n", arr[i][0], arr[i][1]);
+	}
 
 	//자 이제 숫자를 세보자 !!
 	count++;
@@ -49,7 +53,6 @@ int main() {
 		}
 	}
 
-	
 	printf("%d", count);
 
 
