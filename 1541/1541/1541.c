5989a0e26132dd96a92b152d072a4eb201390ba8
unknown <dusdjeks@naver.com>
2021-11-02 15:20:40 +0900

    1541 try

---
+++ 1541/1541/1541.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+	// '-'를 기준으로 나눈다. 나머지는 다 더하면 됨. 그럼 이걸 해결하려면
+	// '-'는 십진법으로 45 !
+
+	char str[50];
+	gets(str);
+
+
+
+
+
+
+
+
+	return 0;
+}
