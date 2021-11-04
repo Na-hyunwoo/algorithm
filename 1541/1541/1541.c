1efff4f85cabbe72c73f52da14887a23ba911a1b
unknown <dusdjeks@naver.com>
2021-11-04 10:58:47 +0900

    1541 try

--- 1541/1541/1541.c
+++ 1541/1541/1541.c
@@ -4,8 +4,6 @@
 int main() {
 	// '-'를 기준으로 나눈다. 나머지는 다 더하면 됨. 그럼 이걸 해결하려면
 	// '-'는 십진법으로 45 !
-	//const char * strchr ( const char * str, int ch ); 문자열에 특정 문자가 있는 위치를 찾는 함수
-	//const char * strrchr ( const char * str, int ch ); 문자열에 특정 문자가 있는 위치를 뒤에서부터 찾는 함수
 	/*
 	1. '-'를 기준으로 문자열을 나눈다. 
 	2. '+"를 기준으로 문자열을 나눈다.
@@ -14,24 +12,46 @@ int main() {
 	*/
 	//c언어로 문자열을 다루는건 너무 힘든 일이야 ㅠ
 
-	char str[50] = { 0, };
-	int i = 0; //인덱스
-	int a[50] = { 0, };
+	char str[50];
+	int temp[50] = { 0, }; //-의 위치를 기억하는 배열
+	int sum[50] = { 0, }; //-로 나눠진 블록들의 합. 
+	int count = 0;//temp의 인덱스
+	int plus[50] = { 0, };// 총 합. 
+	int result = 0; //최종 결과 값
+
 	gets(str);
 
-	while (strchr(str, '-') != NULL) {
-		int i = 0;
-		if(strchr(str,'-')){
-			a[i] = strchr(str, '-');
-			i++;
-			printf("%d", a[i]);
+	printf("str:%s\n", str);
+
+
+	for(int i = 0; i<strlen(str); i++) {
+		if (str[i] == '-') {
+			temp[count] = i;
+			count++;
 		}
 	}
 
+	//-를 기준으로해서 나눠졌으니까. 그 나눠진 부분에 있는 문자열을 모조리 더하는데, 더할 때, 정수형으로 형 변환한다음에 더해야함. 
+	//그럼 쪼개진 부분을 배열로 또 다시 나눠야 하는데, 그걸 또 배열로 만들어서 넣으면 되지. 
 	
+	for (int i = 0; i < temp[0]; i++) {
+		if (str[i] != '+') plus[0] += (int)str[i];
+	}
 
+	if (strlen(temp) >= 2) {
+		for (int i = 0; i < strlen(temp); i++) {
+			for (int j = temp[i] + 1; j < temp[i + 1]; j++) {
+				if (str[i] != '+') plus[i+1] += (int)str[j];
+			}
+		}
+	}
 
-
+	result = plus[0];
+	for (int i = 0; i < strlen(temp); i++) {
+		result -= plus[i + 1];
+	}
+	
+	printf("%d", result);
 
 	return 0;
 }
