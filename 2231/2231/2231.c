b417a75b1c79ce40fe6b6ba0a33c12949e59a20f
unknown <dusdjeks@naver.com>
2021-11-10 10:46:44 +0900

    2231 done

--- 2231/2231/2231.c
+++ 2231/2231/2231.c
@@ -1,42 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//함수를 만들어주니까 코드가 이렇게 간단해지네..  개꿀!!
-//근데 eachNumber에서 시간을 너무 잡아먹어서 새로운 코드가 필요하다. 
-
 int eachNumber(int n) {
-	int res=0;
+	int result=0;
 	while (n != 0) {
-		res = res % 10;
-		res = res / 10;
+		result += n % 10;
+		n /= 10;
 	}
-	return res;
+
+	return result;
 }
 
+
 int main() {
-
-	int* arr;
+	
 	int n;
-	arr = (int*)malloc(sizeof(int) * 1000001);
-	arr[0] = 0;
-	arr[1000000] = 1000001;
-
-	for (int i = 1; i <= 9; i++) {
-		arr[i] = i;
-	}
-
-	for (int i = 10; i <= 999999; i++) {
-		arr[i] = i + eachNumber(i);
-	}
+	int temp; //n을 저장할 임시 저장소 
+	int len=0; //입력받은 정수의 길이
+	int begin = 0; //비교를 시작할 정수 범위
+	int noOne = 1;
 
 	scanf("%d", &n);
 
-	for (int i = 0; i <= 1000000; i++) {
-		if (n == arr[i]) {
+	temp = n;
+	while (temp != 0) {	
+		temp /= 10;
+		len++;
+	}
+
+	begin = n - (9 * len);
+
+
+	for (int i = begin; i <= n; i++) {
+		if (n == (i + eachNumber(i))) {
 			printf("%d", i);
+			noOne = 0;
 			break;
 		}
 	}
+
+	if (noOne) printf("0");
 
 	return 0;
 }
