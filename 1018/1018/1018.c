0fb2295b4e760df70a476708bda5fbee59154fed
unknown <dusdjeks@naver.com>
2021-11-14 11:42:30 +0900

    1018 try3

--- 1018/1018/1018.c
+++ 1018/1018/1018.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+//1을 white, 0을 black으로 만들어볼까. 
 int main() {
 
 	int n, m;
 
 	char** arr=NULL;
-	char** temp=NULL;
+	int** temp=NULL;
 
 	scanf("%d %d", &n, &m);
 
@@ -16,47 +18,43 @@ int main() {
 		arr[i] = (char*)malloc(sizeof(char) * m);
 	}
 	
-	temp = (char*)malloc(sizeof(char*) * n);
+	temp = (int**)malloc(sizeof(int*) * n);
 	for (int i = 0; i < n; i++) {
-		temp[i] = (char*)malloc(sizeof(char) * m);
+		temp[i] = (int*)malloc(sizeof(int) * m);
 	}
 	
-
+	/*
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < m; j++){
 			scanf("%c", &arr[i][j]);
 		}
+	}*/
+	for (int i = 0; i < 8; i++) {
+		for (int j = 0; j < 9; j++) {
+			scanf("%c", &arr[i][j]);
+		}
 	}
-
-
-	//왜 이걸 못하지 바보같이
+	
+	
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < m; j++) {
 			if (i == 0 && j == 0) {
-				temp[0][0] = arr[0][0];
+				if (arr[0][0] == 'W') arr[0][0] = 1;
+				else arr[0][0] = 0;
 			}
 			else if (j != 0 ) {
-				if (temp[i][j - 1] == 'W') {
-					temp[i][j] = 'B';
-				}
-				else if (temp[i][j - 1] == 'B') {
-					temp[i][j] = 'W';
-				}
+				temp[i][j] = !(temp[i][j - 1]);
 			}
 			else {
-				if (temp[i - 1][j] == 'W') {
-					temp[i][j] = 'B';
-				}
-				else if (temp[i - 1][j] == 'B') {
-					temp[i][j] = 'W';
-				}
+				temp[i][j] = !(temp[i-1][j]);
 			}
 		}
 	}
 
+	printf("\n");
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < m; j++) {
-			printf("%c", arr[i][j]);
+			printf("%d", temp[i][j]);
 		}
 		printf("\n");
 	}
